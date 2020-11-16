88ee3047cfd744a82cfaaf4c2036f97a0881acc0
samorenoq <samorenoq@eafit.edu.co>
2020-11-15 23:16:45 -0500

    Mostrar imagen

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -19,6 +19,6 @@ class Controller extends BaseController
         $randomNumber = (rand(1,15));
         $url = '"https://taller2tis.s3.amazonaws.com/';
         echo '<img src=' . $url . $randomNumber . '.jpg" width="500" height="600"/>';
-        echo "<p>server_ip: " . inet_ntop(gethostbyname(gethostname())) . "</p>";
+        echo "<p>server_ip: " . gethostbyname(gethostname()) . "</p>";
     }
 }
